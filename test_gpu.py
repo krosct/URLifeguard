e6acfe55caa64ec2b9fbc651dd4fb2beeffab18b
Gabriel MS <140334417+krosct@users.noreply.github.com>
2025-12-09 14:35:07 -0300

    update test to gpu

--- test_gpu.py
+++ test_gpu.py
@@ -11,3 +11,36 @@ if torch.cuda.is_available():
     print(f"Quantidade de dispositivos: {torch.cuda.device_count()}")
 else:
     print("❌ O PyTorch NÃO está usando a GPU. Ele está rodando na CPU.")
+
+### --- TRASH --- ###
+
+# import sys
+# import os
+
+# # Obtém o caminho absoluto do diretório atual do notebook
+# current_dir = os.getcwd()
+
+# # Obtém o caminho do diretório pai (a pasta URLifeguard)
+# project_root = os.path.dirname(current_dir)
+
+# # Adiciona o diretório pai ao sys.path se ele ainda não estiver lá
+# if project_root not in sys.path:
+#     sys.path.append(project_root)
+
+# # --- Agora você pode importar ---
+
+# # Supondo que seu arquivo seja utils/processamento.py
+# from utils import processamento 
+
+# # Ou se você tem funções específicas dentro dele
+# from utils.processamento import minha_funcao_tokenizacao
+
+# # Carrega a extensão de autoreload
+# %load_ext autoreload
+
+# # Configura para recarregar módulos automaticamente antes de executar o código
+# %autoreload 2
+
+# import sys
+# import os
+# # ... resto do código de importação ...
